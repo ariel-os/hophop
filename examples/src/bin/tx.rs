bc8ac716ff6fe7b97df0968e428e2c5dfd0cdcf7
chrysn <chrysn@fsfe.org>
2026-01-05 23:43:22 +0100

    tx: Send beacon continuously while pressed

--- examples/src/bin/tx.rs
+++ examples/src/bin/tx.rs
@@ -20,30 +20,33 @@ async fn blinky(peripherals: pins::ButtonPeripherals) {
     let button0 = ariel_os::gpio::Input::new(peripherals.button0, ariel_os::gpio::Pull::Up);
 
     loop {
-        // Also gives the pull-up time to actually pull up
+        // Gives the pull-up time to actually pull up
         Timer::after_millis(5).await;
         while button0.is_high() {}
-        info!("Press.");
 
-        dect.tx(
-            0,
-            1665,
-            // FIXME: Not using a proper network ID yet
-            0x12345678,
-            // Beacon as seen by the dect_shell
-            &[17, 120, 150, 24, 112],
-            &[
-                1, 18, 52, 86, 0, 0, 0, 38, 73, 5, 176, 16, 6, 0, 13, 83, 7, 8, 12, 138, 160, 215,
-                2, 100, 64, 24, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                0, 0,
-            ],
-        )
-        .await
-        .unwrap();
+        info!("Press: Starting to transmit.");
+        let mut last_tx = dect.time_get().await.unwrap();
+        const TICKS_PER_FRAME: u64 = 691200;
 
-        // Debounce and wait for release
-        Timer::after_millis(5).await;
-        while button0.is_low() {}
+        while button0.is_low() {
+            last_tx = last_tx.wrapping_add(TICKS_PER_FRAME * 2);
+            dect.tx(
+                last_tx,
+                1665,
+                // FIXME: Not using a proper network ID yet
+                0x12345678,
+                // Beacon as seen by the dect_shell
+                &[17, 120, 150, 24, 112],
+                &[
+                    1, 18, 52, 86, 0, 0, 0, 38, 73, 5, 176, 16, 6, 0, 13, 83, 7, 8, 12, 138, 160, 215,
+                    2, 100, 64, 24, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0,
+                ],
+            )
+            .await
+            .unwrap();
+        }
+        info!("Release: Not sending.");
     }
 
     panic!(
