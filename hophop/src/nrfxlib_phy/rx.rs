36a5d818828112648153d4afff53565ff29c186c
chrysn <chrysn@fsfe.org>
2026-02-25 16:58:13 +0100

    Clean up unused imports

--- hophop/src/nrfxlib_phy/rx.rs
+++ hophop/src/nrfxlib_phy/rx.rs
@@ -1,10 +1,6 @@
 // SPDX-FileCopyrightText: Copyright Christian Amsüss <chrysn@fsfe.org>, Silano Systems
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
-use embassy_sync::{
-    blocking_mutex::raw::CriticalSectionRawMutex,
-    mutex::{Mutex, MutexGuard},
-};
 use heapless::{
     box_pool,
     pool::boxed::{Box, BoxBlock},
