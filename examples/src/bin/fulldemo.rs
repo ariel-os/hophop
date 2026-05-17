dad57f4f5d924df21433778d75b050459d992128
chrysn <chrysn@fsfe.org>
2026-05-17 15:22:08 +0200

    Add debug for nrf MAC layer reported IEs

--- examples/src/bin/fulldemo.rs
+++ examples/src/bin/fulldemo.rs
@@ -39,6 +39,61 @@ mod err {
     }
 }
 
+fn debug_ies(ies: &[nrfxlib_sys::nrf_modem_dect_mac_ie]) {
+    // This'd be easier if we just got the slice of the data
+    for ie in ies {
+        // I'm not fully sold that this is accurate.
+        info!("IE: ie_type {}", ie.ie_type);
+        #[cfg(false)]
+        info!(
+            "data dump: {}",
+            Hex(unsafe {
+                core::slice::from_raw_parts(
+                    &ie.ie as *const _ as *const u8,
+                    core::mem::size_of::<nrfxlib_sys::nrf_modem_dect_mac_ie__bindgen_ty_1>(),
+                )
+            })
+        );
+        match ie.ie_type {
+            nrfxlib_sys::nrf_modem_dect_mac_ie_type_NRF_MODEM_DECT_MAC_IE_TYPE_RD_CAPABILITY => {
+                let details = unsafe { &ie.ie.rd_capability };
+                info!("  RD Capability: group_assign {} paging {} mesh {} scheduled {} dect_delay {} half_dup {} release {} operating_mode {} mac_security {} dlc {}",
+                    details.group_assign_supported,
+                    details.paging_supported,
+                    details.mesh_supported,
+                    details.scheduled_access_supported,
+                    details.dect_delay_supported,
+                    details.half_dup_supported,
+                    details.release,
+                    details.operating_mode,
+                    details.mac_security,
+                    details.dlc_service_type,
+                );
+                info!("    phy cap: power {} max_nss {} ... max_mcs {} ...",
+                    details.phy_capabilities.power_class,
+                    details.phy_capabilities.max_nss,
+                    details.phy_capabilities.max_mcs,
+                );
+            },
+            nrfxlib_sys::nrf_modem_dect_mac_ie_type_NRF_MODEM_DECT_MAC_IE_TYPE_RANDOM_ACCESS_RESOURCE => {
+                let details = unsafe { &ie.ie.random_access_resource };
+                info!("  Random Access Resource: channel {}, response_channel {}, max_tx {}, repetition {}",
+                    unsafe { (&raw const details.channel).read_unaligned() },
+                    unsafe { (&raw const details.response_channel).read_unaligned() },
+                    details.max_rach_tx_length,
+                    details.repetition,
+                );
+                info!("    allocation start {} slots {} length {}",
+                    details.allocation.start_subslot,
+                    details.allocation.use_slots,
+                    unsafe { (&raw const details.allocation.length).read_unaligned() },
+                );
+            },
+            _ => (),
+        }
+    }
+}
+
 mod callbacks {
     #![allow(
         unused_variables,
@@ -147,13 +202,16 @@ mod callbacks {
             nrfxlib_sys::nrf_modem_dect_mac_ipv6_address_type_NRF_MODEM_DECT_MAC_IPV6_ADDRESS_TYPE_PREFIX => "prefix",
             _ => "?"
         }, Hex(params.ipv6_config.address));
+        // FIXME: only valid if bit is set
         info!(
-            "  Association response: ack {}, reject…, flows…, harq…, group_id…, resource_tag…",
+            "  Association response: ack {}, reject…, flows {}, harq…, group_id…, resource_tag…",
             params.association_response.ack_status,
-            // Not showing flows here: that typically gives 0x07 which means "all" … but I hoped to
-            // learn theirs.
+            match params.association_response.number_of_flows {
+                7 => "all that were",
+                _ => "some…",
+            }
         );
-        info!("  IEs: […; {}]", params.number_of_ies);
+        debug_ies(unsafe { core::slice::from_raw_parts(params.ies, params.number_of_ies as _) });
         // Unclear: When is which kind of the flags relevant? Or is this just a type punning union?
     }
     unsafe extern "C" fn association_release(
@@ -287,6 +345,8 @@ mod callbacks {
             params.network_id,
             params.number_of_ies
         );
+        debug_ies(unsafe { core::slice::from_raw_parts(params.ies, params.number_of_ies as _) });
+
         // It's a bit unfortunate that we have to copy around here rather than just re-owning a
         // pool message, but we can still try to do better when we see what's in the actual IPC
         // API.
